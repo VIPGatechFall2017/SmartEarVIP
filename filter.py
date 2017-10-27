c86ad142523684341c1053de433c616fb82a5dc3
Sam Baek <kie827@gmail.com>
2017-10-26 20:05:04 -0400

    try stuff

--- filter.py
+++ filter.py
@@ -1,11 +1,7 @@
 from data_transfer import get_all_img_vectors, txt_to_list
-import plotly_auth
-import plotly
-import plotly.plotly as py
-import plotly.graph_objs as go
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    plotly_auth.AUTH()
     ls = txt_to_list()
     # img = get_all_img_vectors()
 
@@ -17,9 +13,10 @@ if __name__ == "__main__":
         ring = int(int(ring) / 10)
         total[ring] += 1
 
-    data = [go.Bar(
-                x = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
-                y = total
-    )]
+    x = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    y = total
 
-    py.plot(data, filename='basic-bar')
+    print(y)
+
+    plt.bar(x, y)
+    plt.show()
